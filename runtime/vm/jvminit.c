a28f48f4a563c7a85bc0d744a9de62ce4e8d06d6
Graham Chapman <graham_chapman@ca.ibm.com>
2024-06-21 09:39:12 -0400

    revert me
    Signed-off-by: Graham Chapman <graham_chapman@ca.ibm.com>

--- runtime/vm/jvminit.c
+++ runtime/vm/jvminit.c
@@ -1186,7 +1186,7 @@ initializeJavaVM(void * osMainThread, J9JavaVM ** vmPtr, J9CreateJavaVMParams *c
  * Disable AVX+ vector register preservation on x86 due to a large performance regression.
  * Issue: #15716
  */
-#if defined(J9HAMMER) && (JAVA_SPEC_VERSION >= 17) && 0
+#if defined(J9HAMMER)
 	J9ProcessorDesc desc;
 	j9sysinfo_get_processor_description(&desc);
 
