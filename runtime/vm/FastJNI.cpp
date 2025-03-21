9f3b1a4b350dc24f8110aad98989a562b822a021
Graham Chapman <graham_chapman@ca.ibm.com>
2025-03-21 15:35:24 -0400

    Signed-off-by: Graham Chapman <graham_chapman@ca.ibm.com>

--- runtime/vm/FastJNI.cpp
+++ runtime/vm/FastJNI.cpp
@@ -145,6 +145,17 @@ jniNativeMethodProperties(J9VMThread *currentThread, J9Method *jniNativeMethod, 
 						UDATA entryLength = methodDescriptor->methodNameLength;
 						if (J9UTF8_DATA_EQUALS(methodNameData, methodNameLength, entryData, entryLength)) {
 found:
+#if JAVA_SPEC_VERSION >= 24
+							/* Fast implementation located - if wait can unmount the continuation, check to see if this
+							 * native is the wait implememnation and reject it if so.
+							 */
+							if (J9_ARE_ANY_BITS_SET(vm->extendedRuntimeFlags3, J9_EXTENDED_RUNTIME3_YIELD_PINNED_CONTINUATION)
+							&& J9UTF8_LITERAL_EQUALS(methodName, methodNameLength, "waitImpl")
+							&& J9UTF8_LITERAL_EQUALS(methodSignature, methodSignatureLength, "(JI)V")) {
+								goto done;
+							}
+	}
+#endif /* JAVA_SPEC_VERSION >= 24 */
 							address = methodDescriptor->function;
 							flags = methodDescriptor->flags;
 							Trc_VM_fastJNINativeFound(
@@ -223,20 +234,6 @@ found:
 		}
 	}
 done:
-#if JAVA_SPEC_VERSION >= 24
-	if (NULL != address) {
-		/* Fast implementation located - if wait can unmount the continuation, check to see if this
-		 * native is the wait implememnation and reject it if so.
-		 */
-		if (J9_ARE_ANY_BITS_SET(vm->extendedRuntimeFlags3, J9_EXTENDED_RUNTIME3_YIELD_PINNED_CONTINUATION)) {
-			if (J9UTF8_LITERAL_EQUALS_UTF8(J9ROMMETHOD_NAME(romMethod), "waitImpl") && J9UTF8_LITERAL_EQUALS_UTF8(J9ROMMETHOD_SIGNATURE(romMethod), "(JI)V")) {
-				flags = 0;
-				address = NULL;
-			}
-		}
-	}
-#endif /* JAVA_SPEC_VERSION >= 24 */
-
 	*properties = flags;
 	return address;
 }
